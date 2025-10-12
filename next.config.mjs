c9fc8c8fec718237f8fdb43e50423953ccd84522
Rockerto <roberto.ibacache@gmail.com>
2025-10-12 13:02:39 -0300

    chore: Reset build configs to default

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    // Le decimos explícitamente a Next.js que no queremos nada de Turbo
-    turbo: undefined,
-  },
-};
+const nextConfig = {};
 
-export default nextConfig; // <-- ¡ESTE ES EL CAMBIO! Usamos "export default"
+export default nextConfig;
